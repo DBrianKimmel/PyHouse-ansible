e443a76a418666ebb67a66f0acf5f1144fd9c452
DBrianKimmel <d.briankimmel@gmail.com>
2018-03-22 11:23:49 -0400

    Work on mqtt config

---
+++ roles/install-node-red/files/localSettings.js
@@ -0,0 +1,16 @@
+/*
+ * Name:    PyHouse-ansible/roles/install-node-red/files/localSettings.js
+ * Author:  D. Brian Kimmel
+ * Created: 2018-03-21
+ * Updated: 2018-03-21
+ *
+ */
+
+module.exports = {
+    "house_name"       : "pink poppy",
+    "node_name"        : "pi-02",
+    "public_ipv4"      : "1.2.3.4",
+    "connection_lost"  : "Unknown",
+    "front_drip_relay" : "Unknown"
+}
+// ### END DBK
