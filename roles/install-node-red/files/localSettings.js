0368eb8b626e35ada3de360397959b15ed3b189f
DBrianKimmel <d.briankimmel@gmail.com>
2018-07-01 12:46:00 -0400

    Adding

--- roles/install-node-red/files/localSettings.js
+++ roles/install-node-red/files/localSettings.js
@@ -7,7 +7,7 @@
  */
 
 module.exports = {
-    "house_name"       : "{{pyh_housename}}",
+    "house_name"       : "{{pyh_house_name}}",
     "node_name"        : "{{pyh_hostname}}",
     "public_ipv4"      : "1.2.3.4",
     "connection_lost"  : "Unknown",
