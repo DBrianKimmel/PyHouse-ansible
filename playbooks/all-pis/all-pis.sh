8de46ade561ae408c5d07c777a6fed3fafb2ef13
briank <D.BrianKimmel@gmail.com>
2020-12-21 08:17:40 -0500

    WIP

--- playbooks/all-pis/all-pis.sh
+++ playbooks/all-pis/all-pis.sh
@@ -4,7 +4,8 @@ MY_NAME="all-pis"
 
 INVENTORY="-i ./inventories/hosts.yaml "
 PLAYBOOK="playbooks/${MY_NAME}/${MY_NAME}.yaml"
-DEBUG=" "
+DEBUG+" "
+#DEBUG="-vvv "
 
 CUR_DIR=`pwd`
 WORK_DIR=${HOME}
